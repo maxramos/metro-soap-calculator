99935a1c4e346955b9306d2e56253e47019d6b96
max <max@max-msi>
2017-01-04 04:38:32 +0800

    Added files

---
+++ src/main/java/ph/maxramos/metro/soap/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package ph.maxramos.metro.soap.calculator;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+public class Calculator {
+
+	public int add(@WebParam(name="addend1") int addend1, @WebParam(name="addend2") int addend2) {
+		return addend1 + addend2;
+	}
+
+}
---
+++ src/main/java/ph/maxramos/metro/soap/calculator/listener/StartUpListener.java
@@ -0,0 +1,20 @@
+package ph.maxramos.metro.soap.calculator.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class StartUpListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		//		HttpAdapter.dump = true;
+		//		HttpAdapter.dump_threshold = 8192;
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+	}
+
+}
