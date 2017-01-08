0e3408b95d237b9ba83e945e8f33a7dba0bd2ae4
max <max@max-msi>
2017-01-09 00:50:15 +0800

    added handler

--- src/main/java/ph/maxramos/metro/soap/calculator/Calculator.java
+++ src/main/java/ph/maxramos/metro/soap/calculator/Calculator.java
@@ -1,9 +1,11 @@
 package ph.maxramos.metro.soap.calculator;
 
+import javax.jws.HandlerChain;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
+@HandlerChain(file = "handlers.xml")
 public class Calculator {
 
 	public int add(@WebParam(name="addend1") int addend1, @WebParam(name="addend2") int addend2) {
